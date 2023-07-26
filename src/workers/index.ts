59708404e1a70b4f5b418a2aab34918d8e59c959
Кирилл Самылин <kirill-itb@yandex.ru>
2023-07-26 14:46:27 +0300

    fix: refactor worker

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -1,3 +1,1 @@
- let tabControlWorker = new SharedWorker(new URL('./tab-control.js', import.meta.url));
-
- export { tabControlWorker }
+ export const tabControlWorker = new SharedWorker(new URL('./tab-control.js', import.meta.url));
